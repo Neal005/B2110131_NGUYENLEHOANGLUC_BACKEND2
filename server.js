1c4bf7bf19594a8475ac9792a4ddcd4086bc88f3
Neal Nguyen <hluc2003@gmail.com>
2023-10-03 14:38:26 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -3,13 +3,16 @@ const config = require("./app/config");
 const MongoDB = require("./app/utils/mongodb.util");
 async function startServer() {
     try {
-        await MongoDB.connect(config.db.uri); console.log("Connected to the database!");
+        await MongoDB.connect(config.db.uri);
+        console.log("Connected to the database!");
         const PORT = config.app.port;
         app.listen(PORT, () => {
-            (`console.log(Server is running on port ${PORT}`);
+            console.log(`Server is running on port ${PORT}.`);
         });
     } catch (error) {
-        console.log("Cannot connect to the database!", error); process.exit();
+        console.log("Cannot connect to the database!", error);
+        process.exit();
     }
 }
+
 startServer();
